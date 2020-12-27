763210d5cd34bff5cc0430ec6b5d7be3fe6afecf
YasNihBos <76633756+YasNihBos@users.noreply.github.com>
2020-12-27 10:11:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ async function start() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `Sayonara 👋`
+				teks = `GOOD BYEE TOD👋`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks})
 			}
